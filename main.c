1a5fb5dcb70299d948dd496831e04656e747dc51
Haden Collins <collinshaden@gmail.com>
2019-12-29 20:10:00 -0600

    Refactor code to be cleaner

--- main.c
+++ main.c
@@ -5,39 +5,41 @@
 #include <gtk/gtk.h>
 #include "jsmn.h"
 
-#define EXCLUSIVE 2
-#define MAX_SIZE 1024
-
-static int margin = 500;
+static const int exclusive_level = 2;
+static const int default_size = 100;
 
 typedef struct
 {
-    char label[MAX_SIZE];
-    char action[MAX_SIZE];
-    char text[MAX_SIZE];
+    char *label;
+    char *action;
+    char *text;
 } button;
 
-static char command[MAX_SIZE];
-static button buttons[MAX_SIZE];
-static int num_buttons = 0;
-static int buttons_per_row = 3;
+static char *command;
+static button *buttons;
 static GtkWidget *gtk_window;
 
+static int num_buttons = 0;
+static int buttons_per_row = 3;
+static int margin = 500;
+
 static GtkWidget *get_window();
-static char *get_string(char *s, int start, int end, char *buf);
+static char *get_substring(char *s, int start, int end, char *buf);
 static gboolean get_buttons(FILE *json);
 static void display_buttons(GtkWindow *window);
 static void load_css();
 
 int main (int argc, char *argv[])
 {
+    buttons = malloc(sizeof(button) * default_size);
+
     g_set_prgname("wlogout");
     gtk_init(&argc, &argv);
     
     FILE *inptr = fopen(argv[1], "r");
     if (!inptr)
     {
-        fprintf(stderr, "Failed to open %s\n", argv[1]);
+        g_warning("Failed to open %s\n", argv[1]);
         return 2;
     }
     if (get_buttons(inptr))
@@ -49,18 +51,22 @@ int main (int argc, char *argv[])
     gtk_window = get_window();
     gtk_container_set_border_width(GTK_CONTAINER(gtk_window), margin);
     display_buttons(GTK_WINDOW(gtk_window));
+    free(buttons);
+
     load_css();
     gtk_widget_show_all(gtk_window);
 
     gtk_main();
-
     system(command);
+
+    free(command);
 }
 
 static void execute(GtkWidget *widget, char *action)
 {
+    command = malloc(strlen(action) * sizeof(char) + 1);
     strcpy(command, action);
-    gtk_widget_hide(gtk_window);
+    gtk_widget_destroy(gtk_window);
     gtk_main_quit();
 }
 
@@ -109,24 +115,29 @@ static gboolean get_buttons(FILE *json)
     char *buffer = malloc(length);
     if (!buffer)
     {
-        fprintf(stderr, "Failed to allocate memory\n");
+        g_warning("Failed to allocate memory\n");
         return TRUE;
     }
     fread(buffer, 1, length, json);
 
     jsmn_parser p;
-    jsmntok_t *tok = malloc(MAX_SIZE);
+    jsmntok_t *tok = malloc(default_size);
     if (!tok)
     {
-        fprintf(stderr, "Failed to allocate memory\n");
+        g_warning("Failed to allocate memory\n");
         return TRUE;
     }
     jsmn_init(&p);
-    int numtok = jsmn_parse(&p, buffer, length, tok, MAX_SIZE);
+    int numtok, i = 1;
+    do
+    {
+        numtok = jsmn_parse(&p, buffer, length, tok, default_size * i);
+        i++;
+    } while (numtok == JSMN_ERROR_NOMEM);
 
     if (numtok < 0)
     {
-        fprintf(stderr, "Failed to parse json data\n");
+        g_warning("Failed to parse json data\n");
         return TRUE;
     }
     
@@ -138,37 +149,45 @@ static gboolean get_buttons(FILE *json)
         }
         else if (tok[i].type == JSMN_STRING)
         {
-            char tmp[tok[i].end - tok[i].start + 1];
-            get_string(tmp, tok[i].start, tok[i].end, buffer);
+            int length = tok[i].end - tok[i].start;
+            char tmp[length + 1];
+            get_substring(tmp, tok[i].start, tok[i].end, buffer);
             i++;
+            length = tok[i].end - tok[i].start;
 
             if (strcmp(tmp, "label") == 0)
             {
-                char buf[tok[i].end - tok[i].start + 1];
-                get_string(buf, tok[i].start, tok[i].end, buffer);
+                char buf[length + 1];
+                get_substring(buf, tok[i].start, tok[i].end, buffer);
+                buttons[num_buttons - 1].label = malloc(sizeof(char)
+                        * length + 1);
                 strcpy(buttons[num_buttons - 1].label, buf);
             }
             else if (strcmp(tmp, "action") == 0)
             {
-                char buf[tok[i].end - tok[i].start + 1];
-                get_string(buf, tok[i].start, tok[i].end, buffer);
+                char buf[length + 1];
+                get_substring(buf, tok[i].start, tok[i].end, buffer);
+                buttons[num_buttons - 1].action = malloc(sizeof(char)
+                        * length + 1);
                 strcpy(buttons[num_buttons - 1].action, buf);
             }
             else if (strcmp(tmp, "text") == 0)
             {
-                char buf[tok[i].end - tok[i].start + 1];
-                get_string(buf, tok[i].start, tok[i].end, buffer);
+                char buf[length + 1];
+                get_substring(buf, tok[i].start, tok[i].end, buffer);
+                buttons[num_buttons - 1].text = malloc(sizeof(char)
+                        * length + 1);
                 strcpy(buttons[num_buttons - 1].text, buf);
             }
             else
             {
-                fprintf(stderr, "Invalid key\n");
+                g_warning("Invalid key %s\n", tmp);
                 return TRUE;
             }
         }
         else
         {
-            fprintf(stderr, "Invalid JSON Data\n");
+            g_warning("Invalid JSON Data\n");
             return TRUE;
         }
     }
@@ -176,10 +195,11 @@ static gboolean get_buttons(FILE *json)
     free(tok);
     free(buffer);
     fclose(json);
+
     return FALSE;
 }
 
-char *get_string(char *s, int start, int end, char *buf)
+char *get_substring(char *s, int start, int end, char *buf)
 {
     memcpy(s, &buf[start], (end - start));
     s[end - start] = '\0';
@@ -191,7 +211,7 @@ static GtkWidget *get_window()
     GtkWindow *window = GTK_WINDOW (gtk_window_new (GTK_WINDOW_TOPLEVEL));
     gtk_layer_init_for_window (window);
     gtk_layer_set_layer (window, GTK_LAYER_SHELL_LAYER_OVERLAY);
-    gtk_layer_set_exclusive_zone (window, EXCLUSIVE);
+    gtk_layer_set_exclusive_zone (window, exclusive_level);
     gtk_layer_set_keyboard_interactivity (window, FALSE);
 
     static const gboolean anchors[] = {TRUE, TRUE, TRUE, TRUE};
@@ -205,7 +225,14 @@ static GtkWidget *get_window()
 static void load_css()
 {
     GtkCssProvider *css = gtk_css_provider_new();
-    gtk_css_provider_load_from_path(css, "/usr/local/etc/wlogout/style.css", NULL);
+    GError *error = NULL;
+    gtk_css_provider_load_from_path(css, "/usr/local/etc/wlogout/style.css",
+            &error);
+    if (error)
+    {
+        g_warning("%s", error->message);
+        g_clear_error(&error);
+    }
     gtk_style_context_add_provider_for_screen(gdk_screen_get_default(),
-                GTK_STYLE_PROVIDER(css), GTK_STYLE_PROVIDER_PRIORITY_USER);
+            GTK_STYLE_PROVIDER(css), GTK_STYLE_PROVIDER_PRIORITY_USER);
 }
